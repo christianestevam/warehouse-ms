9beb8d33aba35f79b87fd26b83a73057812075a0
Christian Estevam <christianestevamb@gmail.com>
2023-04-01 17:27:44 -0300

    fix: modules

---
+++ src/main/java/com/gump/warehousems/dto/WarehouseDTO.java
@@ -0,0 +1,10 @@
+package com.gump.warehousems.dto;
+
+import lombok.Data;
+
+@Data
+public class WarehouseDTO {
+
+    private Long productId;
+    private int quantity;
+}
---
+++ src/main/java/com/gump/warehousems/model/Warehouse.java
@@ -0,0 +1,22 @@
+package com.gump.warehousems.model;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "tb_warehouse")
+@Data
+public class Warehouse {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "id_product", nullable = false)
+    private Long productId;
+
+    @Column(name = "quantity")
+    private int quantity;
+}
